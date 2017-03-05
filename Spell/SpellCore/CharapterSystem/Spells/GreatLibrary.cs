b648c03a3dd937fd721bc561bd740642b0447b9e
Elmion <firegeometry@gmail.com>
2017-03-06 00:18:28 +1000

    1.111

---
+++ Spell/SpellCore/CharapterSystem/Spells/GreatLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellCore.CharapterSystem
+{
+    //Единый центр для заклинаний
+    class GreatLibrary
+    {
+        internal static void StartCastSpell(string nameSpell, object owner, object target)
+        {
+
+        }
+
+        internal static void InterruptCastingSpell(string nameSpell, object owner)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Spell/SpellCore/Core.cs
+++ Spell/SpellCore/Core.cs
@@ -7,8 +7,13 @@ using System.Threading.Tasks;
 
 namespace SpellCore
 {
+    /// <summary>
+    /// Единая точка входа для стороних систем
+    /// Пока преимущественно расматриваем вариант jRPG как самый простой варинат кастовки заклинаний
+    /// </summary>
     class Core
     {
+        
         public static bool CastSpell(string nameSpell,object owner,object target)
         {
             if (owner is BaseCharapter && target is BaseCharapter)
@@ -18,9 +23,38 @@ namespace SpellCore
                 if(o.Card.hasSpell(nameSpell) && o.isSee(t,nameSpell) && o.inRange(t,nameSpell))//тут будут ещё условия например на дальность.
                 {
                     //Здесь сам каст пока не знаю как будет выглядеть
+                    GreatLibrary.StartCastSpell(nameSpell, owner, target);
                 }
             }
            return true;
+        }
+        public static bool InterruptCastingSpell(string nameSpell, object owner)
+        {
+            if (owner is BaseCharapter)
+            {
+                BaseCharapter o = owner as BaseCharapter;
+                if (o.Card.hasSpell(nameSpell))
+                {
+                    //Здесь сам каст пока не знаю как будет выглядеть
+                    GreatLibrary.InterruptCastingSpell(nameSpell, owner);
+                }
+            }
+            return true;
+        }
+        //Возможно нужно будет ещё одна ветка методов с удалением без активирования последствий
+        //но пока что так.
+        public static bool RemoveSpellFromObject(string nameSpell, object target)
+        {
+            if (target is BaseCharapter)
+            {
+                BaseCharapter t = target as BaseCharapter;
+                if (t.Card.hasSpellEffect(nameSpell))//тут будут ещё условия например на дальность.
+                {
+                    //Здесь сам каст пока не знаю как будет выглядеть
+                    GreatLibrary.Dispell(nameSpell, target);
+                }
+            }
+            return true;
         }
     }
 }
