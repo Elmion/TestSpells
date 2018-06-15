66086df42a3a56b708804651041e984cb8ea02b2
Elmion <iight@bk.ru>
2018-06-15 16:37:31 +1000

    asdasd

--- MouseHeart/MouseHeart/Form1.Designer.cs
+++ MouseHeart/MouseHeart/Form1.Designer.cs
@@ -28,335 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pbHp = new System.Windows.Forms.ProgressBar();
-            this.pbHangry = new System.Windows.Forms.ProgressBar();
-            this.pbStamina = new System.Windows.Forms.ProgressBar();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.tbLungs = new System.Windows.Forms.TextBox();
-            this.tbBlood = new System.Windows.Forms.TextBox();
-            this.tbPancreas = new System.Windows.Forms.TextBox();
-            this.tbLiver = new System.Windows.Forms.TextBox();
-            this.tbHeart = new System.Windows.Forms.TextBox();
-            this.tbBrain = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
+            this.bEat = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // pbHp
+            // bEat
             // 
-            this.pbHp.Location = new System.Drawing.Point(65, 15);
-            this.pbHp.Name = "pbHp";
-            this.pbHp.Size = new System.Drawing.Size(423, 12);
-            this.pbHp.TabIndex = 0;
-            // 
-            // pbHangry
-            // 
-            this.pbHangry.Location = new System.Drawing.Point(65, 35);
-            this.pbHangry.Name = "pbHangry";
-            this.pbHangry.Size = new System.Drawing.Size(423, 12);
-            this.pbHangry.TabIndex = 0;
-            // 
-            // pbStamina
-            // 
-            this.pbStamina.Location = new System.Drawing.Point(65, 55);
-            this.pbStamina.Name = "pbStamina";
-            this.pbStamina.Size = new System.Drawing.Size(423, 12);
-            this.pbStamina.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 14);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(22, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "HP";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 34);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Hangry";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 55);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 13);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Stamina";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(504, 7);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(42, 20);
-            this.textBox1.TabIndex = 2;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(504, 27);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(42, 20);
-            this.textBox2.TabIndex = 2;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(504, 47);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(42, 20);
-            this.textBox3.TabIndex = 2;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.label12);
-            this.groupBox1.Controls.Add(this.label11);
-            this.groupBox1.Controls.Add(this.label10);
-            this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.tbLungs);
-            this.groupBox1.Controls.Add(this.tbBlood);
-            this.groupBox1.Controls.Add(this.tbPancreas);
-            this.groupBox1.Controls.Add(this.tbLiver);
-            this.groupBox1.Controls.Add(this.tbHeart);
-            this.groupBox1.Controls.Add(this.tbBrain);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Location = new System.Drawing.Point(12, 84);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(166, 203);
-            this.groupBox1.TabIndex = 3;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Физика";
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(6, 154);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(36, 13);
-            this.label12.TabIndex = 1;
-            this.label12.Text = "Lungs";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(6, 130);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(34, 13);
-            this.label11.TabIndex = 1;
-            this.label11.Text = "Blood";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(6, 103);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(52, 13);
-            this.label10.TabIndex = 1;
-            this.label10.Text = "Рancreas";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 76);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(30, 13);
-            this.label6.TabIndex = 1;
-            this.label6.Text = "Liver";
-            // 
-            // tbLungs
-            // 
-            this.tbLungs.Location = new System.Drawing.Point(95, 153);
-            this.tbLungs.Name = "tbLungs";
-            this.tbLungs.ReadOnly = true;
-            this.tbLungs.Size = new System.Drawing.Size(56, 20);
-            this.tbLungs.TabIndex = 2;
-            // 
-            // tbBlood
-            // 
-            this.tbBlood.Location = new System.Drawing.Point(95, 127);
-            this.tbBlood.Name = "tbBlood";
-            this.tbBlood.ReadOnly = true;
-            this.tbBlood.Size = new System.Drawing.Size(56, 20);
-            this.tbBlood.TabIndex = 2;
-            // 
-            // tbPancreas
-            // 
-            this.tbPancreas.Location = new System.Drawing.Point(95, 99);
-            this.tbPancreas.Name = "tbPancreas";
-            this.tbPancreas.ReadOnly = true;
-            this.tbPancreas.Size = new System.Drawing.Size(56, 20);
-            this.tbPancreas.TabIndex = 2;
-            // 
-            // tbLiver
-            // 
-            this.tbLiver.Location = new System.Drawing.Point(95, 73);
-            this.tbLiver.Name = "tbLiver";
-            this.tbLiver.ReadOnly = true;
-            this.tbLiver.Size = new System.Drawing.Size(56, 20);
-            this.tbLiver.TabIndex = 2;
-            // 
-            // tbHeart
-            // 
-            this.tbHeart.Location = new System.Drawing.Point(95, 46);
-            this.tbHeart.Name = "tbHeart";
-            this.tbHeart.ReadOnly = true;
-            this.tbHeart.Size = new System.Drawing.Size(56, 20);
-            this.tbHeart.TabIndex = 2;
-            // 
-            // tbBrain
-            // 
-            this.tbBrain.Location = new System.Drawing.Point(95, 20);
-            this.tbBrain.Name = "tbBrain";
-            this.tbBrain.ReadOnly = true;
-            this.tbBrain.Size = new System.Drawing.Size(56, 20);
-            this.tbBrain.TabIndex = 2;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 51);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(33, 13);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "Heart";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 27);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(31, 13);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Brain";
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.label7);
-            this.groupBox2.Controls.Add(this.label8);
-            this.groupBox2.Controls.Add(this.label9);
-            this.groupBox2.Location = new System.Drawing.Point(184, 84);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(158, 203);
-            this.groupBox2.TabIndex = 3;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox1";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(6, 103);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(45, 13);
-            this.label7.TabIndex = 1;
-            this.label7.Text = "Stamina";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(6, 61);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(45, 13);
-            this.label8.TabIndex = 1;
-            this.label8.Text = "Stamina";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(6, 27);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(45, 13);
-            this.label9.TabIndex = 1;
-            this.label9.Text = "Stamina";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(432, 84);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Открыть формулы";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.bEat.Location = new System.Drawing.Point(55, 33);
+            this.bEat.Name = "bEat";
+            this.bEat.Size = new System.Drawing.Size(59, 54);
+            this.bEat.TabIndex = 0;
+            this.bEat.Text = "Еда";
+            this.bEat.UseVisualStyleBackColor = true;
+            this.bEat.Click += new System.EventHandler(this.bEat_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(558, 299);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pbStamina);
-            this.Controls.Add(this.pbHangry);
-            this.Controls.Add(this.pbHp);
+            this.Controls.Add(this.bEat);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ProgressBar pbHp;
-        private System.Windows.Forms.ProgressBar pbHangry;
-        private System.Windows.Forms.ProgressBar pbStamina;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox tbLungs;
-        private System.Windows.Forms.TextBox tbBlood;
-        private System.Windows.Forms.TextBox tbPancreas;
-        private System.Windows.Forms.TextBox tbLiver;
-        private System.Windows.Forms.TextBox tbHeart;
-        private System.Windows.Forms.TextBox tbBrain;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button bEat;
     }
 }
 
--- MouseHeart/MouseHeart/Form1.cs
+++ MouseHeart/MouseHeart/Form1.cs
@@ -7,56 +7,279 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using LeopotamGroup.Ecs;
 namespace MouseHeart
 {
+
     public partial class Form1 : Form
     {
+        public static EcsWorld world;
+        public static EcsSystems systems;
         Timer t = new Timer();
-        EffectManager mng = new EffectManager();
+        public static bool EatClick = false;
         public Form1()
         {
             InitializeComponent();
-            mng.Initialize();
-            t.Interval = 1;
             t.Tick += T_Tick;
-           // t.Start();
-            //Coefficient.Add("x");
-            //Coefficient.ListCoefficients["x"].Value = 4;
-            //Coefficient.Add("y");
-            //Coefficient.ListCoefficients["y"].Value = 8;
-            СalculationFormula f = new СalculationFormula("((2+ydf/2)/(xc+2+xc+x))");
-            // Console.WriteLine(f.Calc());
+            world = new EcsWorld();
+            systems = new EcsSystems(world)
+                            .Add(new LungsProcessing())
+                            .Add(new EatProcessing())
+                            .Add(new DigestiondProcessing())
+                            .Add(new HeartProcessing())
+                            .Add(new BrainProcessing())
+                            .Add(new MuscleProcessing())
+                            .Add(new BonesProcessing())
+                            .Add(new ConsoleOutputProcessing());
 
-            //using (FormulaEditor form = new FormulaEditor(f))
-            //{
-            //    form.ShowDialog();
-            //}
+            systems.Initialize();
 
-            ////Varible.LoadVaribles();
-            //  var d = Varible.ListVaribles;
-            f["ydf"] = 8;
-            f["xc"] = 1;
-            f["x"] = 1;
-            Console.WriteLine(f.Calc());
-            f["ydf"] = 3;
-            f["xc"] = 2;
-            f["x"] = 3;
-            
-            Console.WriteLine(f.Calc());
-            Console.ReadLine();
+            int i = world.CreateEntity();
+            var f = world.AddComponent<Mouse>(i);
+            f.blood = new Blood();
+            f.blood.V3 = 5.0f;
+            f.stomach = new Stomach();
+            f.blood.energy = new Energy();
+            f.blood.o2 = new O2();
+            f.heart = new Heart();
+            f.heart.V3 = 0.02f;
+            f.heart.Frequence = 2f;
+            f.stomach.StomachCells = new List<Food>();
+            f.lungs = new Lungs();
+            f.lungs.Concentration = new O2();
+            f.lungs.Concentration.Ammont = 0.1f;
+            f.lungs.V3 = 3;
+            f.lungs.Frequence = 6;
+
+            i = world.CreateEntity();
+
+
+            t.Start();
         }
         private void T_Tick(object sender, EventArgs e)
         {
-            mng.Update();
-            Console.Clear();
-            Console.WriteLine(mng.MouseOrganStatus());
-            Console.SetCursorPosition(0, 0);
+            systems.Run();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void bEat_Click(object sender, EventArgs e)
         {
-            //
+           EatClick = true;
         }
+    }
+    public class Mouse
+    {
+        public Stomach stomach;
+        public Blood blood;
+        public Heart heart;
+        public Lungs lungs;
+        
+    }
+    public class Stomach
+    {
+       public List<Food> StomachCells;
+    }
+    public class Lungs
+    {
+        public O2 Concentration;
+        public float V3;
+        public float Frequence;
+    }
+    public class Heart
+    {
+        public float V3;
+        public float Frequence;
+    }
+    public class Food
+    {
+        public Energy energy;
+        public float TimeDigestion;
+    }
+    public class Blood
+    {
+        public float V3;
+        public Energy energy;
+        public O2 o2;
+    }
+    public class Energy
+    {
+        public float Amount;
+    }
+    public class O2
+    {
+        public float Ammont;
+    }
+
+    [EcsInject]
+    public class LungsProcessing :  IEcsRunSystem
+    {
+        EcsWorld world = null;
+        EcsFilter<Mouse> mouse = null;
+
+        public void Run()
+        {
+            for (int i = 0; i < mouse.EntitiesCount; i++)
+            {
+                var m = mouse.Components1[i];
+                if(m.blood.o2.Ammont <= 0)
+                {
+                    m.lungs.Frequence = Math.Abs(m.blood.o2.Ammont) / (m.lungs.V3 * m.lungs.Concentration.Ammont);
+                    m.blood.o2.Ammont += (float)Math.Ceiling(m.lungs.Frequence) * m.lungs.V3 * m.lungs.Concentration.Ammont;
+                }
+            }
+        }
+        public void Destroy() { }
+    }
+    [EcsInject]
+    public class DigestiondProcessing : IEcsInitSystem, IEcsRunSystem
+    {
+        EcsWorld world = null;
+        EcsFilter<Mouse> mouse = null;
+
+        public void Run()
+        {
+            for (int i = 0; i < mouse.EntitiesCount; i++)
+            {
+                var m = mouse.Components1[i];
+                m.blood.energy.Amount += GetEnergy(m.stomach);
+            }
+        }
+        public void Initialize()
+        {
+        }
+        public void Destroy() {}
+
+        float GetEnergy(Stomach stomach)
+        {
+            float energyOut = 0;
+            for (int i = 0; i < stomach.StomachCells.Count; i++)
+            {
+                if (stomach.StomachCells[i].energy.Amount > 0)
+                {
+                    stomach.StomachCells[i].energy.Amount--;
+                    energyOut ++;
+                }
+            }
+            return energyOut;
+        }
+    }
+    [EcsInject]
+    public class HeartProcessing : IEcsRunSystem
+    {
+        EcsWorld world = null;
+        EcsFilter<Mouse> mouse = null;
+
+        public void Run()
+        {
+            for (int i = 0; i < mouse.EntitiesCount; i++)
+            {
+                var m = mouse.Components1[i];
+                m.blood.o2.Ammont += m.blood.o2.Ammont / (m.blood.V3/ (m.heart.V3*m.heart.Frequence));
+                m.blood.energy.Amount -= 1f;
+            }
+        }
+        public void Destroy() { }
+    }
+    [EcsInject]
+    public class BrainProcessing : IEcsRunSystem
+    {
+        EcsWorld world = null;
+        EcsFilter<Mouse> mouse = null;
+
+        public void Run()
+        {
+            for (int i = 0; i < mouse.EntitiesCount; i++)
+            {
+                var m = mouse.Components1[i];
+                m.blood.o2.Ammont -= 0.2f;// m.blood.o2.Ammont / (m.blood.V3 / (m.heart.V3 * m.heart.Frequence));
+                m.blood.energy.Amount -= 1f;
+            }
+        }
+        public void Destroy() { }
+    }
+    [EcsInject]
+    public class MuscleProcessing : IEcsRunSystem
+    {
+        EcsWorld world = null;
+        EcsFilter<Mouse> mouse = null;
+        public void Run()
+        {
+            for (int i = 0; i < mouse.EntitiesCount; i++)
+            {
+                var m = mouse.Components1[i];
+                m.blood.o2.Ammont -= 1f;// m.blood.o2.Ammont / (m.blood.V3 / (m.heart.V3 * m.heart.Frequence));
+                m.blood.energy.Amount -= 1f;
+            }
+        }
+        public void Destroy() { }
+    }
+    [EcsInject]
+    public class BonesProcessing : IEcsRunSystem
+    {
+        EcsWorld world = null;
+        EcsFilter<Mouse> mouse = null;
+
+        public void Run()
+        {
+            for (int i = 0; i < mouse.EntitiesCount; i++)
+            {
+                var m = mouse.Components1[i];
+                m.blood.o2.Ammont -= 1f;// m.blood.o2.Ammont / (m.blood.V3 / (m.heart.V3 * m.heart.Frequence));
+                m.blood.energy.Amount -= 1f;
+            }
+        }
+        public void Destroy() { }
+    }
+    [EcsInject]
+    public class ConsoleOutputProcessing : IEcsRunSystem
+    {
+        EcsWorld world = null;
+        EcsFilter<Mouse> mouse = null;
+
+        public void Run()
+        {
+            for (int i = 0; i < mouse.EntitiesCount; i++)
+            {
+                var m = mouse.Components1[i];
+                Console.Clear();
+                Console.WriteLine("e  " + m.blood.energy.Amount);
+                Console.WriteLine("o2 " + m.blood.o2.Ammont);
+                string s = "";
+                for (int j = 0; j < m.stomach.StomachCells.Count; j++)
+                {
+                    s += m.stomach.StomachCells[j].energy.Amount + " ";
+                }
+                Console.WriteLine("St " + s);
+            }
+        }
+        public void Destroy() { }
+    }
+    [EcsInject]
+    public class EatProcessing : IEcsRunSystem
+    {
+        EcsWorld world = null;
+        EcsFilter<Mouse> mouse = null;
+
+        public void Run()
+        {
+            for (int i = 0; i < mouse.EntitiesCount; i++)
+            {
+                var m = mouse.Components1[i];
+                if (m.stomach.StomachCells.Count < 5 && Form1.EatClick)
+                {
+                    m.stomach.StomachCells.Insert(0, new Food() { energy = new Energy() });
+                    m.stomach.StomachCells[0].energy.Amount = 200;
+                    Form1.EatClick = false;
+                }
+                for (int j = 0; j < m.stomach.StomachCells.Count; j++)
+                {
+                    if (m.stomach.StomachCells[j].energy.Amount <= 0)
+                    {
+                        m.stomach.StomachCells.Remove(m.stomach.StomachCells[j]);
+                    }
+                }
+            }
+        }
+        public void Destroy() { }
+
+
     }
 }
